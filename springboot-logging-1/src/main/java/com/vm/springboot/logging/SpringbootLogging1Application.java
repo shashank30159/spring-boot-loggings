1733c45b9862a94ce6bfaf95737af83a1c08d5e0
SHASHANK <shashankbompally@gmail.com>
2021-06-23 16:36:44 +0530

    initial commit

---
+++ springboot-logging-1/src/main/java/com/vm/springboot/logging/SpringbootLogging1Application.java
@@ -0,0 +1,13 @@
+package com.vm.springboot.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLogging1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLogging1Application.class, args);
+	}
+
+}
---
+++ springboot-logging-1/src/test/java/com/vm/springboot/logging/SpringbootLogging1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.springboot.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLogging1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
